65ac0ba370dcd5edfa16293b329203515e268b72
AnastasiiaOvs <a.alexandrovna@i.ua>
2025-05-20 13:37:55 +0200

    main.js moved

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { getImagesByQuery } from './pixabay-api.js';
+import { getImagesByQuery } from './js/pixabay-api.js';
 import {
   createGallery,
   clearGallery,
   showLoader,
   hideLoader,
   scrollToNewImages,
-} from './render-functions.js';
+} from './js/render-functions.js';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
